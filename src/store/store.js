69a6830aa48bdc39f1844dd0d28380b91a34ac7e
刘伟伟(10020854) <liuww-a@glodon.com>
2019-12-15 16:01:14 +0800

    unit3

--- src/store/store.js
+++ src/store/store.js
@@ -7,4 +7,13 @@ const storeReducers = combineReducers({
     index: IndexStore
 });
 
-export default new createStore(storeReducers, applyMiddleware(thunk));
+// export default new createStore(storeReducers, applyMiddleware(thunk));
+
+export const getServerStore = () => {
+    return new createStore(storeReducers, applyMiddleware(thunk));
+}
+
+export const getClientStore = () => {
+    const defaultState = window.__context ? window.__context : {}
+    return new createStore(storeReducers, defaultState, applyMiddleware(thunk))
+}
